e0f24f3b3076877f7f5ec0d0930a6cca5727853e
AdmiralDarius <DariusSafavy@gmail.com>
2025-01-05 23:11:58 +0000

    include Llama3 and other models in the test file

--- BQAandCOT/test.py
+++ BQAandCOT/test.py
@@ -3,15 +3,10 @@ import pandas as pd
 parser = argparse.ArgumentParser(description='Test LLMs for bayesian infernece')
 parser.add_argument('--testdataset', dest='testdataset', default="../datasets/Colored_1000_examples.csv", help='input test dataset',type=str)
 parser.add_argument('--outputdataset', dest='outputdataset', default="../datasets/", help='Dataset folder that has saved the results',type=str)
-parser.add_argument('--models', dest='models',default="all", choices=["gpt-3.5-turbo-0613", "gpt-4-0613","all"], help="Choose a model")
+parser.add_argument('--models',dest='models',nargs='+',default=["gpt-3.5-turbo", "gpt-4-0613","meta/meta-llama-3-70b-instruct","mistralai/mistral-7b-instruct-v0.2", "meta/llama-2-70b-chat"],choices=["gpt-3.5-turbo", "gpt-4-0613","meta/meta-llama-3-70b-instruct","mistralai/mistral-7b-instruct-v0.2", "meta/llama-2-70b-chat"], help="Specify one or more models.")
 parser.add_argument('--CLADDER', dest='CLADDER', action='store_true', help='use CLADDER dataset', default=False)
 
 args = parser.parse_args()
-
-if args.models=="all":
-    LLM_models=["gpt-3.5-turbo", "gpt-4"]
-else:
-    LLM_models = [args.models]
 
 if args.CLADDER:
     args.testdataset="../datasets/CLADDER_test.csv"
@@ -23,14 +18,17 @@ def asnwer_extractor(response):
     numbers = re.findall(pattern, response)
     return float(numbers[-1])
 
-for model_name in LLM_models:
+for model_name in args.models:
+    model_name_folder=model_name
+    if "/" in model_name:
+        model_name_folder=model_name.split("/")[1]
     for args_method in ["BQA","COT"]:
         for few_shot in [False,True]:
         
             if args.CLADDER:
-                file_name = args.outputdataset+str(args_method)+"_model_name "+model_name+"_few_shot_"+str(few_shot)+"_CLADDER.csv"
+                file_name = args.outputdataset+str(args_method)+"_model_name "+model_name_folder+"_few_shot_"+str(few_shot)+"_CLADDER.csv"
             else:
-                file_name = args.outputdataset+str(args_method)+"_model_name "+model_name+"_few_shot_"+str(few_shot)+".csv"
+                file_name = args.outputdataset+str(args_method)+"_model_name "+model_name_folder+"_few_shot_"+str(few_shot)+".csv"
             if not os.path.exists(file_name):
                 continue
             print(model_name)
--- PALandMC/test.py
+++ PALandMC/test.py
@@ -4,7 +4,7 @@ from utils import extract_sections
 parser = argparse.ArgumentParser(description='Test LLMs for bayesian infernece')
 parser.add_argument('--testdataset', dest='testdataset', default="../datasets/Colored_1000_examples.csv", help='input test dataset',type=str)
 parser.add_argument('--outputdataset', dest='outputdataset', default="../datasets/", help='Dataset folder that has saved the results',type=str)
-parser.add_argument('--models', dest='models',default="all", choices=["gpt-3.5-turbo-0613", "gpt-4-0613","all"], help="Choose a model")
+parser.add_argument('--models',dest='models',nargs='+',default=["gpt-3.5-turbo", "gpt-4-0613","meta/meta-llama-3-70b-instruct","mistralai/mistral-7b-instruct-v0.2", "meta/llama-2-70b-chat"],choices=["gpt-3.5-turbo", "gpt-4-0613","meta/meta-llama-3-70b-instruct","mistralai/mistral-7b-instruct-v0.2", "meta/llama-2-70b-chat"], help="Specify one or more models.")
 parser.add_argument('--method', dest='method', default="PAL",choices=["PAL", "MC"], help='method to solve the problem')
 parser.add_argument('--CLADDER', dest='CLADDER', action='store_true', help='use CLADDER dataset', default=False)
 
@@ -13,10 +13,6 @@ args = parser.parse_args()
 if args.CLADDER:
     args.testdataset="../datasets/CLADDER_test.csv"
 
-if args.models=="all":
-    LLM_models=["gpt-3.5-turbo-0613", "gpt-4-0613"]
-else:
-    LLM_models = [args.models]
 data = pd.read_csv(args.testdataset)
 
 def parse_graph1(graph_string):
@@ -73,14 +69,17 @@ def extract_import_random_section(text):
     else:
         return ""
 
-for model_name in LLM_models:
+for model_name in args.models:
+    model_name_folder=model_name
+    if "/" in model_name:
+        model_name_folder=model_name.split("/")[1]
     for number_use in [False,True]:
         for graph_use in [False,True]:
         
             if args.CLADDER:
-                file_name = args.outputdataset+str(args.method)+"_model_name "+model_name+" graph included "+str(graph_use)+" number use "+str(number_use)+"_CLADDER.csv"
+                file_name = args.outputdataset+str(args.method)+"_model_name "+model_name_folder+" graph included "+str(graph_use)+" number use "+str(number_use)+"_CLADDER.csv"
             else:
-                file_name = args.outputdataset+str(args.method)+"_model_name "+model_name+" graph included "+str(graph_use)+" number use "+str(number_use)+".csv"
+                file_name = args.outputdataset+str(args.method)+"_model_name "+model_name_folder+" graph included "+str(graph_use)+" number use "+str(number_use)+".csv"
             if not os.path.exists(file_name):
                 continue
 
